f3d98ec4bb9d54a7f6f10aeb23cfc84a291c0cec
Aman Verma <amanverma03328@gmail.com>
2022-04-05 23:15:25 +0530

    feat: preview of image in upload post page

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,10 +1,13 @@
 let date = new Date();
 let months = ["January", "February", "March", "April","May","June", "July", "August", "September", "October", "November", "December"];
-let day = ["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"]
+let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
+let imageToUploadPreviewBox = document.getElementById("imageToUploadPreviewBox");
+let imageToUploadPreview = document.getElementById("imageToUploadPreview");
 let overlayBox = document.getElementById("overlayBox");
 let todaysDate = document.getElementById("todaysDate");
 let actualImageInput = document.getElementById("actualImageInput");
+let reselectButton = document.getElementById("reselectButton");
 let progressBarActualProgress = document.getElementById("progressBarActualProgress");
 
 // initial function call
@@ -18,25 +21,39 @@ dateUpdate();
 // Event Listener
 
 overlayBox.addEventListener("click",triggerImageSelect);
-actualImageInput.addEventListener("change",uploadProgress);
+reselectButton.addEventListener("click",triggerImageSelect);
+actualImageInput.addEventListener("change",showPreview);
+
 
 // Event Listener end
 
 
 function triggerImageSelect(){
-    // alert("hello");
     actualImageInput.click();
-
-
-    // uploadProgress();
-
-    // code to display:none overlay box
-    overlayBox.classList.remove("d-flex");
-    overlayBox.classList.add("d-none");
 }
 
 function dateUpdate(){
     todaysDate.innerText = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}, ${day[date.getDay()]}`;
+}
+
+function showPreview(){
+    let fileReader = new FileReader();
+    let imageToUpload = actualImageInput.files[0];
+
+    fileReader.readAsDataURL(imageToUpload);
+
+    fileReader.onload = function(){
+        // fileReader.result gives the base64 image
+        // console.log(fileReader.result);
+
+        imageToUploadPreview.src = fileReader.result;
+    }
+
+    // code to display:none overlay box
+    overlayBox.classList.remove("d-flex");
+    overlayBox.classList.add("d-none");
+
+    reselectButton.style.display = "block";
 }
 
 
@@ -45,7 +62,6 @@ function uploadProgress(){
     console.log(actualImageInput.files);
 
     let t = setInterval(function (){
-        // console.log("chala");
         progressBarActualProgress.style.width = `${width+1}%`;
         width+=1;
         if(width>100){
