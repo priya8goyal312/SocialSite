de9621aa2d7d401ba9b26f8594eb855255dc0950
amverma-systango <amverma@isystango.com>
2022-04-07 17:11:31 +0530

    feat: added emoji picker functionality

---
+++ static/js/chat.js
@@ -0,0 +1,29 @@
+let messageInputBox = document.getElementById("messageInputBox");
+let emojiBox = document.getElementById("emojiBox");
+
+
+
+// Event Listener
+
+// Event Listener end
+
+
+// initial calls
+emojiBuilder();
+// initial calls end
+
+// functions
+window.appendEmoji = function(emojiCode){
+    messageInputBox.value = messageInputBox.value + emojiCode;
+}
+
+
+function emojiBuilder(){
+    let emojiBoxInnerHTML = "";
+
+    for(let emojiCodeItr = 128512 ; emojiCodeItr <= 129488; emojiCodeItr++){
+        emojiBoxInnerHTML += `<div class="p-2" onclick="appendEmoji('&#${emojiCodeItr}')">&#${emojiCodeItr}</div>`;
+    }
+    emojiBox.innerHTML = emojiBoxInnerHTML;
+}
+// functions end
